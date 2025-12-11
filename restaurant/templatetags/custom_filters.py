2cf2894e47d309ac17b8a354b2453714d639781e
Mathieuleboss <163544288+Mathieuleboss@users.noreply.github.com>
2025-12-11 15:13:46 +0100

    Add split and index filters to custom_filters.py
    Added custom filters to split strings and retrieve list items by index.

--- restaurant/templatetags/custom_filters.py
+++ restaurant/templatetags/custom_filters.py
@@ -5,3 +5,17 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def split(value, arg):
+    """Divise une chaîne en liste"""
+    return value.split(arg)
+
+@register.filter
+def index(list_obj, i):
+    """Récupère un élément d'une liste par son index"""
+    try:
+        return list_obj[int(i)]
+    except (IndexError, TypeError, ValueError):
+        return ''
